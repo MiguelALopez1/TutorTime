e5e70cad4e1a1e1a0642b867e7d949cb4cc00974
miguellpz <miguellpz@vt.edu>
2023-11-30 18:32:09 -0500

    added register file

--- TutorTime/app/src/main/java/com/example/tutortime/RegisterFragment.kt
+++ TutorTime/app/src/main/java/com/example/tutortime/RegisterFragment.kt
@@ -1,32 +1,26 @@
 package com.example.tutortime
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -34,26 +28,49 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        val createAccountButton: Button = view.findViewById(R.id.createAccount)
+        createAccountButton.setOnClickListener { createAccount(view) }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun createAccount(view: View) {
+        val emailEditText: EditText = view.findViewById(R.id.email)
+        val passwordEditText: EditText = view.findViewById(R.id.password)
+
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        // Validate inputs
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(context, "Email and password cannot be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Create a new user with Firebase Authentication
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("RegisterFragment", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("RegisterFragment", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
                 }
             }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            findNavController().navigate(R.id.launchFragment)
+        } else {
+            Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
