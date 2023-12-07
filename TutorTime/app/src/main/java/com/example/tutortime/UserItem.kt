2205984c01539c2c5bd97408bb8ba0ba6ec6b6ee
Jazz Jackson <jazzjacksonhomebase@gmail.com>
2023-12-06 20:13:09 -0500

    Updates the UserItem dataclass to take the new Settings dataclasses

--- TutorTime/app/src/main/java/com/example/tutortime/UserItem.kt
+++ TutorTime/app/src/main/java/com/example/tutortime/UserItem.kt
@@ -1,3 +1,4 @@
 package com.example.tutortime
 
-data class UserItem(var name: String, var userId: String, var student: Boolean)
+data class UserItem(var name: String, var userId: String, var student: Boolean,
+                    var studentSettings: StudentSettings, var tutorSettings: TutorSettings)
